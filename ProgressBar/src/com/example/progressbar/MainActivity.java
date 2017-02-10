8588adfc3ec7e7067825653552d232b68b9acd01
AndroidLuo666 <1107680857@qq.com>
2017-02-10 11:25:19 +0800

    ProgressBar
    android ProgressBar 绿色圆形进度条 demo

---
+++ ProgressBar/src/com/example/progressbar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.progressbar;
+
+import com.example.actionbar.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
